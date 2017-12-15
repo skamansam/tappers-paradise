2707c3a92ce9b38b03c661d714fa114d7b74ebea
Samuel C. Tyler <skamansam@gmail.com>
2017-12-15 12:10:06 -0500

    use chrome headless instead of phantom

--- src/lib/numbers.js
+++ src/lib/numbers.js
@@ -1,26 +1,41 @@
+// let NumberClass = require('bignumber.js')
+// NumberClass.config({ DECIMAL_PLACES: 15, ROUNDING_MODE: BigNumber.ROUND_DOWN })
+let NumberClass = require('decimal.js')
+
 export const NEW_GREEK = 0
 export const SHORT = 1
 export const LONG = 2
+export const INTL = 3
 
 export function humanReadable (n, scale = SHORT) {
+
+  let num = new NumberClass(n)
   switch (scale) {
     case NEW_GREEK:
-      return customScale(n, newGreekScale)
+      return customScale(num, newGreekScale)
     case SHORT:
-      return customScale(n, shortScale)
+      return customScale(num, shortScale)
     case LONG:
-      return customScale(n, longScale)
+      return customScale(num, longScale)
+    case INTL:
+      return customScale(num, longIntlScale)
     default:
-      return customScale(n, scale)
+      return customScale(num, scale)
   }
 }
 export default humanReadable
 
 export function _places (n) {
-  if (n < 10) {
+  let num = n
+  if (!(n instanceof NumberClass)) {
+    // console.error('NOT a BigNumber!')
+    num = new NumberClass(n)
+  }
+  if (num < 10) {
     return 0
   }
-  return Math.floor(Math.log10(Math.abs(n)).toFixed(10))
+  debugger
+  return Math.floor(Math.log10(Math.abs(num)).toFixed(10))
 }
 
 export function customScale (n, scaleFunction) {
@@ -57,9 +72,90 @@ function newGreekScale (n, len) {
   return result
 }
 
-function shortScale (n) {
-  return n
+function shortScale (n, len) {
+  if (n === undefined || n < 1e3) {
+    return ''
+  }
+  // const maxNumber = 1e93 - 1
+  // const maxDigits = 92
+  const idx = Math.floor(len / 3)
+  // short scale - US, Canada, Modern British
+  const lowerNumbers = [ '',
+    'Thousand', 'Million', 'Billion', 'Trillion', 'Quadrillion', 'Quintillion',
+    'Sextillion', 'Septillion', 'Octillion', 'Nonillion', 'Decillion', 'Undecillion',
+    'Duodecillion', 'Tredecillion', 'Quattuordecillion', 'Quinquadecillion',
+    'Sedecillion', 'Septendecillion', 'Octodecillion', 'Novendecillion', 'Vigintillion',
+    'Unvigintillion', 'Duovigintillion', 'Tresvigintillion', 'Quattuorvigintillion',
+    'Quinquavigintillion', 'Sesvigintillion', 'Septemvigintillion', 'Octovigintillion',
+    'Novemvigintillion'
+  ]
+  let result = lowerNumbers[idx]
+  if (result === undefined) {
+    let newLen = len - 93
+    let triplets = newLen / 3
+    const calcPrefixes = ['', 'Un', 'duo', 'tres', 'quattuor', 'quinqua', 'ses', 'septen', 'octo', 'noven']
+    const calcSuffixes = ['Trigintillion', 'Quadragintillion', 'Quinquagintillion', 'Sexagintillion',
+      'Septuagintillion', 'Octogintillion', 'Nonagintillion', 'Centillion',
+      'Decicentillion', 'Undecicentillion', 'Viginticentillion', 'Unviginticentillion',
+      'Trigintacentillion', 'Quadragintacentillion', 'Quinquagintacentillion',
+      'Sexagintacentillion', 'Septuagintacentillion', 'Octogintacentillion',
+      'Nonagintacentillion', 'Ducentillion', 'Trecentillion', 'Quadringentillion',
+      'Quingentillion', 'Sescentillion', 'Septingentillion', 'Octingentillion',
+      'Nongentillion', 'Millinillion'
+    ]
+    // console.log(len, new_len, triplets, triplets % 10, Math.floor(triplets / 10))
+    result = '' + calcPrefixes[triplets % 10] + '' + calcSuffixes[Math.floor(triplets / 10)]
+  }
+  result = result.toLowerCase()
+  result = result.charAt(0).toUpperCase() + result.slice(1)
+  return result
 }
-function longScale (n) {
-  return n
+
+function longScale (n, len) {
+  // long scale - Traditional British
+  const dict = [ '',
+    'Thousand', 'Million', 'Thousand million', 'Billion', 'Thousand billion', 'Trillion', 'Thousand trillion',
+    'Quadrillion', 'Thousand quadrillion', 'Quintillion', 'Thousand quintillion', 'Sextillion',
+    'Thousand sextillion', 'Septillion', 'Thousand septillion', 'Octillion', 'Thousand octillion',
+    'Nonillion', 'Thousand nonillion', 'Decillion', 'Thousand decillion', 'Undecillion',
+    'Thousand undecillion', 'Duodecillion', 'Thousand duodecillion', 'Tredecillion',
+    'Thousand tredecillion', 'Quattuordecillion', 'Thousand quattuordecillion', 'Quindecillion',
+    'Thousand quindecillion', 'Sedecillion', 'Thousand sedecillion', 'Septendecillion',
+    'Thousand septendecillion', 'Octodecillion', 'Thousand octodecillion', 'Novendecillion',
+    'Thousand novendecillion', 'Vigintillion', 'Thousand vigintillion',
+    'Thousand quinquavigintillion', 'Thousand trigintillion', 'Thousand quinquatrigintillion',
+    'Thousand quadragintillion', 'Thousand quinquaquadragintillion', 'Thousand quinquagintillion',
+    'Unquinquagintillion', 'Thousand unquinquagintillion', 'Duoquinquagintillion',
+    'Thousand quinquaquinquagintillion', 'Sesquinquagintillion', 'Thousand sexagintillion',
+    'Unsexagintillion', 'Thousand quinquasexagintillion', 'Thousand septuagintillion',
+    'Thousand quinquaseptuagintillion', 'Thousand octogintillion', 'Thousand quinquaoctogintillion',
+    'Thousand nonagintillion', 'Thousand quinquanonagintillion', 'Thousand centillion',
+    'Thousand quinquagintacentillion', 'Thousand ducentillion', 'Thousand quinquagintaducentillion',
+    'Thousand trecentillion', 'Thousand quinquagintatrecentillion', 'Thousand quadringentillion',
+    'Thousand quinquagintaquadringentillion', 'Thousand quingentillion'
+  ]
+  return dict[n / 3]
+}
+
+function longIntlScale (n) {
+  // long scale - traditional european
+  const dict = ['',
+    'Thousand', 'Million', 'Milliard', 'Billion', 'Billiard', 'Trillion', 'Trilliard', 'Quadrillion',
+    'Quadrilliard', 'Quintillion', 'Quintilliard', 'Sextillion', 'Sextilliard', 'Septillion',
+    'Septilliard', 'Octillion', 'Octilliard', 'Nonillion', 'Nonilliard', 'Decillion',
+    'Decilliard', 'Undecillion', 'Undecilliard', 'Duodecillion', 'Duodecilliard', 'Tredecillion',
+    'Tredecilliard', 'Quattuordecillion', 'Quattuordecilliard', 'Quindecillion', 'Quindecilliard',
+    'Sedecillion', 'Sedecilliard', 'Septendecillion', 'Septendecilliard', 'Octodecillion',
+    'Octodecilliard', 'Novendecillion', 'Novendecilliard', 'Vigintillion', 'Vigintilliard',
+    'Quinquavigintilliard', 'Trigintilliard', 'Quinquatrigintilliard', 'Quadragintilliard',
+    'Quinquaquadragintilliard', 'Quinquagintilliard', 'Unquinquagintillion',
+    'Unquinquagintilliard', 'Duoquinquagintillion', 'Quinquaquinquagintilliard',
+    'Sesquinquagintillion', 'Sexagintilliard', 'Unsexagintillion', 'Quinquasexagintilliard',
+    'Septuagintilliard', 'Quinquaseptuagintilliard', 'Octogintilliard', 'Quinquaoctogintilliard',
+    'Nonagintilliard', 'Quinquanonagintilliard', 'Centilliard', 'Quinquagintacentilliard',
+    'Ducentilliard', 'Quinquagintaducentilliard', 'Trecentilliard',
+    'Quinquagintatrecentilliard', 'Quadringentilliard', 'Quinquagintaquadringentilliard',
+    'Quingentilliard'
+  ]
+  return dict[n / 3]
 }
--- test/unit/specs/number.spec.js
+++ test/unit/specs/number.spec.js
@@ -2,8 +2,8 @@ import * as Numbers from '../../../src/lib/numbers.js'
 
 describe('numbers.js', () => {
   describe('New Greek System', () => {
-    describe('0 - 1.00e3 should print nothing', () => {
-      it('0 - 10 should have 0 places', () => {
+    describe('GREEK: 0 - 1.00e3 should print nothing', () => {
+      it('GREEK: 0 - 10 should have 0 places', () => {
         for (let i = 0; i < 10; i = i + 1) {
           let res = Numbers.humanReadable(i, Numbers.NEW_GREEK)
           expect(Numbers._places(i)).to.equal(0)
@@ -11,7 +11,7 @@ describe('numbers.js', () => {
           expect(res).to.equal('')
         }
       })
-      it('10 - 99 should have 1 place', () => {
+      it('GREEK: 10 - 99 should have 1 place', () => {
         for (let i = 10; i < 100; i = i + 1) {
           let res = Numbers.humanReadable(i, Numbers.NEW_GREEK)
           expect(Numbers._places(i)).to.equal(1)
@@ -20,17 +20,17 @@ describe('numbers.js', () => {
         }
       })
     })
-    describe('1.00e3 - 1.00e6 should print Thousand', () => {
+    describe('GREEK: 1.00e3 - 1.00e6 should print Thousand', () => {
       for (let exp = 3; exp < 6; exp++) {
-        it('1.00e' + exp +' to 1.00e' + (exp + 1) +' should have ' + exp + ' places', () => {
-          for (let i = parseFloat('1.00e' + exp); i < parseFloat('1.00e' + (exp+1)); i = i + exp) {
+        it('1.00e' + exp + ' to 1.00e' + (exp + 1) + ' should have ' + exp + ' places', () => {
+          for (let i = parseFloat('1.00e' + exp); i < parseFloat('1.00e' + (exp + 1)); i = exp - 3 + i + exp) {
             expect(Numbers._places(i)).to.equal(exp)
             expect(Numbers.humanReadable(i, Numbers.NEW_GREEK)).to.equal('Thousand')
           }
         })
       }
     })
-    describe('1.00e6 - 1.00e9 should print Million', () => {
+    describe('GREEK: 1.00e6 - 1.00e9 should print Million', () => {
       for (let exp = 6; exp < 9; exp++) {
         it('1.00e' + exp + ' to 1.00e' + (exp + 1) + ' should have ' + exp + ' places', () => {
           for (let i = parseFloat('1.00e' + exp); i < parseFloat('1.00e' + (exp + 1)); i = i + 17 + Math.pow(exp - 6)) {
@@ -154,6 +154,309 @@ describe('numbers.js', () => {
       let res = Numbers.humanReadable(1.00e96, Numbers.NEW_GREEK)
       expect(res).to.be.a('string')
       expect(res).to.equal('Thousand Triacontillion')
+    })
+  })
+  describe('Short Scale (English [US/British])', () => {
+    describe('0 - 1.00e3 should print nothing', () => {
+      it('0 - 10 should have 0 places', () => {
+        for (let i = 0; i < 10; i = i + 1) {
+          let res = Numbers.humanReadable(i, Numbers.SHORT)
+          expect(res).to.be.a('string')
+          expect(res).to.equal('')
+        }
+      })
+      it('10 - 99 should have 1 place', () => {
+        for (let i = 10; i < 100; i = i + 1) {
+          let res = Numbers.humanReadable(i, Numbers.SHORT)
+          expect(res).to.be.a('string')
+          expect(res).to.equal('')
+        }
+      })
+      it('100 - 999 should have 2 places', () => {
+        for (let i = 100; i < 1000; i = i + 1) {
+          let res = Numbers.humanReadable(i, Numbers.SHORT)
+          expect(res).to.be.a('string')
+          expect(res).to.equal('')
+        }
+      })
+    })
+    describe('0 - 1.00e3 should print nothing', () => {
+      it('0 - 10 should have 0 places', () => {
+        for (let i = 0; i < 10; i = i + 1) {
+          let res = Numbers.humanReadable(i, Numbers.SHORT)
+          expect(res).to.be.a('string')
+          expect(res).to.equal('')
+        }
+      })
+      it('10 - 99 should have 1 place', () => {
+        for (let i = 10; i < 100; i = i + 1) {
+          let res = Numbers.humanReadable(i, Numbers.SHORT)
+          expect(res).to.be.a('string')
+          expect(res).to.equal('')
+        }
+      })
+      it('100 - 999 should have 2 places', () => {
+        for (let i = 100; i < 1000; i = i + 1) {
+          let res = Numbers.humanReadable(i, Numbers.SHORT)
+          expect(res).to.be.a('string')
+          expect(res).to.equal('')
+        }
+      })
+    })
+    describe('SHORT: 1.00e3 - 1.00e6 should print Thousand', () => {
+      for (let exp = 3; exp < 6; exp++) {
+        it('1.00e' + exp + ' to 1.00e' + (exp + 1) + ' should have ' + exp + ' places', () => {
+          for (let i = parseFloat('1.00e' + exp); i < parseFloat('1.00e' + (exp + 1)); i = i + exp + exp - 3) {
+            expect(Numbers._places(i)).to.equal(exp)
+            expect(Numbers.humanReadable(i, Numbers.SHORT)).to.equal('Thousand')
+          }
+        })
+      }
+    })
+    describe('1.00e6 - 1.00e9 should print Million', () => {
+      for (let exp = 6; exp < 9; exp++) {
+        it('1.00e' + exp + ' to 1.00e' + (exp + 1) + ' should have ' + exp + ' places', () => {
+          for (let i = parseFloat('1.00e' + exp); i < parseFloat('1.00e' + (exp + 1)); i = i + 17 + Math.pow(exp - 6)) {
+            expect(Numbers._places(i)).to.equal(exp)
+            expect(Numbers.humanReadable(i, Numbers.SHORT)).to.equal('Million')
+          }
+        })
+      }
+    })
+    it('should print Billion', () => {
+      let res = Numbers.humanReadable(1.00e9, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Billion')
+    })
+    it('should print Trillion', () => {
+      let res = Numbers.humanReadable(1.00e12, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Trillion')
+    })
+    it('should print Trillion', () => {
+      let res = Numbers.humanReadable(1.00e13, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Trillion')
+    })
+    it('should print Trillion', () => {
+      let res = Numbers.humanReadable(1.00e14, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Trillion')
+    })
+    it('should print Quadrillion', () => {
+      let res = Numbers.humanReadable(1.00e15, Numbers.SHORT)
+      expect(Numbers._places(1.00e15)).to.equal(15)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quadrillion')
+    })
+    it('should print Quadrillion', () => {
+      let res = Numbers.humanReadable(1.00e15 + 5, Numbers.SHORT)
+      expect(Numbers._places(1.00e15 + 5)).to.equal(15)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quadrillion')
+    })
+    it('should print Quadrillion', () => {
+      let res = Numbers.humanReadable(1.00e15 + 25, Numbers.SHORT)
+      expect(Numbers._places(1.00e15 + 25)).to.equal(15)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quadrillion')
+    })
+    it('should print Quadrillion', () => {
+      let res = Numbers.humanReadable(1.00e15 + 27, Numbers.SHORT)
+      expect(Numbers._places(1.00e15 + 27)).to.equal(15)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quadrillion')
+    })
+    it('should print Quadrillion', () => {
+      let res = Numbers.humanReadable(1.00e16 + 27, Numbers.SHORT)
+      expect(Numbers._places(1.00e16 + 27)).to.equal(16)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quadrillion')
+    })
+    it('should print Quintillion', () => {
+      let res = Numbers.humanReadable(1.00e18, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quintillion')
+    })
+    it('should print Sextillion', () => {
+      let res = Numbers.humanReadable(1000000000000000000000.00, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Sextillion')
+    })
+    it('should print Septillion', () => {
+      let res = Numbers.humanReadable(1000000000000000000000000.00, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Septillion')
+    })
+    it('should print Octillion', () => {
+      let res = Numbers.humanReadable(1000000000000000000000000000.00, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Octillion')
+    })
+    it('should print Undecillion', () => {
+      let res = Numbers.humanReadable(1000000000000000000000000000000000000.00, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Undecillion')
+    })
+    it('should print Quattuordecillion', () => {
+      let res = Numbers.humanReadable(1000000000000000000000000000000000000000000000.00, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quattuordecillion')
+    })
+    it('should print Septendecillion', () => {
+      let res = Numbers.humanReadable(1000000000000000000000000000000000000000000000000000000.00, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Septendecillion')
+    })
+    it('should print Vigintillion', () => {
+      let res = Numbers.humanReadable(1000000000000000000000000000000000000000000000000000000000000000.00, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Vigintillion')
+    })
+    it('should print Novemvigintillion', () => {
+      let res = Numbers.humanReadable(1.00e90, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Novemvigintillion')
+    })
+    it('should print Novemvigintillion', () => {
+      let res = Numbers.humanReadable(1.00e91, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Novemvigintillion')
+    })
+    it('should print Novemvigintillion', () => {
+      let res = Numbers.humanReadable(1.00e92, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Novemvigintillion')
+    })
+    it('should print Trigintillion', () => {
+      let res = Numbers.humanReadable(1.00e93, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Trigintillion')
+    })
+    it('should print Untrigintillion', () => {
+      let res = Numbers.humanReadable(1.00e96, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Untrigintillion')
+    })
+    it('should print Duotrigintillion', () => {
+      let res = Numbers.humanReadable(1.00e99, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Duotrigintillion')
+    })
+    it('should print Trestrigintillion', () => {
+      let res = Numbers.humanReadable(1.00e102, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Trestrigintillion')
+    })
+    it('should print Quattuortrigintillion', () => {
+      let res = Numbers.humanReadable(1.00e105, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quattuortrigintillion')
+    })
+    it('should print Quinquatrigintillion', () => {
+      let res = Numbers.humanReadable(1.00e108, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quinquatrigintillion')
+    })
+    it('should print Sestrigintillion', () => {
+      let res = Numbers.humanReadable(1.00e111, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Sestrigintillion')
+    })
+    it('should print Septentrigintillion', () => {
+      let res = Numbers.humanReadable(1.00e114, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Septentrigintillion')
+    })
+    it('should print Octotrigintillion', () => {
+      let res = Numbers.humanReadable(1.00e117, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Octotrigintillion')
+    })
+    it('should print Noventrigintillion', () => {
+      let res = Numbers.humanReadable(1.00e120, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Noventrigintillion')
+    })
+    it('should print Quadragintillion', () => {
+      let res = Numbers.humanReadable(1.00e123, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quadragintillion')
+    })
+    it('should print Quinquagintillion', () => {
+      let res = Numbers.humanReadable(1.00e153, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quinquagintillion')
+    })
+    it('should print Sexagintillion', () => {
+      let res = Numbers.humanReadable(1.00e183, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Sexagintillion')
+    })
+    it('should print Septuagintillion', () => {
+      let res = Numbers.humanReadable(1.00e213, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Septuagintillion')
+    })
+    it('should print Octogintillion', () => {
+      let res = Numbers.humanReadable(1.00e243, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Octogintillion')
+    })
+    it('should print Nonagintillion', () => {
+      let res = Numbers.humanReadable(1.00e273, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Nonagintillion')
+    })
+    it('should print Centillion', () => {
+      let res = Numbers.humanReadable(1.00e303, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Centillion')
+    })
+    it('should print Uncentillion', () => {
+      let res = Numbers.humanReadable(1.00e306, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Uncentillion')
+    })
+    it('should print Duocentillion', () => {
+      let res = Numbers.humanReadable('1.00e309', Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Duocentillion')
+    })
+    it('should print Trescentillion', () => {
+      let res = Numbers.humanReadable('1.00e312', Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Trescentillion')
+    })
+    it('should print Decicentillion', () => {
+      let res = Numbers.humanReadable(1.00e333, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Decicentillion')
+    })
+    it('should print Undecicentillion', () => {
+      let res = Numbers.humanReadable(1.00e336, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Undecicentillion')
+    })
+    it('should print Viginticentillion', () => {
+      let res = Numbers.humanReadable(1.00e363, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Viginticentillion')
+    })
+    it('should print Unviginticentillion', () => {
+      let res = Numbers.humanReadable(1.00e366, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Unviginticentillion')
+    })
+    it('should print Trigintacentillion', () => {
+      let res = Numbers.humanReadable(1.00e393, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Trigintacentillion')
+    })
+    it('should print Quadragintacentillion', () => {
+      let res = Numbers.humanReadable(1.00e423, Numbers.SHORT)
+      expect(res).to.be.a('string')
+      expect(res).to.equal('Quadragintacentillion')
     })
   })
 })
