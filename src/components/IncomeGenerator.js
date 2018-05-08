f2787006e415dafc4b6e12f31d6f13d342befd31
Samuel C. Tyler <skamansam@gmail.com>
2018-05-08 10:15:27 -0400

    add click handling to generators

--- src/components/IncomeGenerator.js
+++ src/components/IncomeGenerator.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import Card from 'material-ui/Card';
 //import Typography from 'material-ui/Typography';
 //import { withStyles } from 'material-ui/styles';
 import themeElement from '../themeElement';
-
+//import Paper from 'material-ui/Paper';
 class IncomeGenerator extends Component {
+
+  clickie() {
+    console.log('Calling clickie()');
+    this.props.onClickie('HI!');
+    this.props.nonono();
+    console.log('Called clickie()');
+  }
   render() {
+    const { data } = this.props;
     return (
-      <div>
-        <h1>Generator!</h1>
-      </div>
+      <Card onClick={ () => { this.clickie() } }>
+        <h1>Generator: {data && data.name}</h1>
+      </Card>
     );
   }
 }
